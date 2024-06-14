5a4c0db1fa8cd346ac2571af0dc4c05fbec36f05
“Aliaksei <at13091986@gmail.comgit config --global user.email at13091986@gmail.com>
2024-06-14 14:32:29 +0200

    Added search page

--- presentation/Store.Web/Program.cs
+++ presentation/Store.Web/Program.cs
@@ -1,25 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+startup.Configure(app, app.Environment);
 
 app.Run();
