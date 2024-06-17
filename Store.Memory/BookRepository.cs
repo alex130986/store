b477ac00bf6972f5f9c51b542b66431270b54081
“Aliaksei <at13091986@gmail.comgit config --global user.email at13091986@gmail.com>
2024-06-17 10:22:11 +0200

    Small changes

--- Store.Memory/BookRepository.cs
+++ Store.Memory/BookRepository.cs
@@ -4,9 +4,9 @@
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "Art Of Programming"),
-            new Book(2, "Refactoring"),
-            new Book(3, "C Programming Language"),
+            new Book(1, "ISBN 12312-31231", "D. Knuth", "Art Of Programming"),
+            new Book(2, "ISBN 12312-31232", "M. Fowler", "Refactoring"),
+            new Book(3, "ISBN 12312-31231", "B. Kernighan, D. Ritchie", "C Programming Language"),
         };
 
         public Book[] GetAllByTitle(string titlePart)
